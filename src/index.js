d07ba1062ce60318299c05571489597e2ec76694
Cheng Pham <chengpham@gmail.com>
2020-12-14 11:35:54 -0800

    updated homepage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import $ from 'jquery';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// window.addEventListener('load', () => {
+//   const preload = document.querySelector('.preload')
+//   preload.classList.add('preload-finish')
+// })
+
+const slider = (
+  <AwesomeSlider>
+    <div class="container-fluid">
+      <div class='app'>
+        <img id="avatar" src="./avatar.png"/>
+        <h1>Hi, my name is Cheng Pham.</h1>
+        <h1>I build things for the web.</h1>
+        <p>
+          I am a full-stack web developer and UI/UX javascript specialist
+          based in Vancouver, BC. I specialize in designing websites and application.
+          Check out projects on here to see my latest projects and live demos of the work I've done.
+        </p>
+      </div>
+      
+    </div>
+
+    <div class="container-fluid">
+      <div class='app'>
+          <h1>My Portfolio</h1>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class='app'>
+          <h1>Contact Me</h1>
+      </div>
+    </div>
+  </AwesomeSlider>
 );
+class App extends React.Component {
+  constructor(props){
+      super(props)
+  }
+  render(){
+      return (
+          <div id='main'>
+            {slider}
+            <div id='nav'>
+              <nav>
+                <ul>
+                  <li><a href="http://chengpham.com"> <i class="fas fa-globe-americas"></i></a> </li>
+                  <li><a href="http://github.com/chengpham"> <i class="fab fa-github"></i></a> </li>
+                  <li><a href="http://linkedin.com/in/chengpham"> <i class="fab fa-linkedin-in"></i></a> </li>
+                  <li><a href="#"> <i class="fas fa-at"></i></a> </li>
+                </ul>
+              </nav>
+            </div>
+            <div class="preload">
+              <div class="loader"><img src="./avatar_head.png" /></div>
+            </div>
+          </div>
+      )
+  }
+}
+ReactDOM.render( <App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
