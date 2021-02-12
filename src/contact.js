d7626e29ebd2df82053e76668385807e1857255b
Cheng Pham <chengpham@gmail.com>
2021-02-11 21:30:41 -0800

    update

---
+++ src/contact.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import * as emailjs from 'emailjs-com';
+
+import { Field, Label, Control, Input, Button, Textarea} from 'rbx';
+
+class ContactForm extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', subject: '', message: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, email, subject, message } = this.state;
+    console.log(this.state)
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'cheng',
+      subject,
+      message_html: message,
+    };
+    emailjs.send( 'service_5lhgvmk', 'template_yuu79ag', templateParams, 'user_xyFHlx2eg6bbAezEi6Dbu' )
+    this.resetForm();
+  };
+
+  resetForm() { this.setState({ name: '', email: '', subject: '',  message: '' }) }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    const { name, email, subject, message } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Field>
+          <Label>Name</Label>
+          <Control>
+            <Input name="name" type="text" placeholder="Your name" value={name} 
+            onChange={this.handleChange} />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Email</Label>
+          <Control>
+            <Input name="email" type="email" placeholder="email@gmail.com" value={email}
+              onChange={this.handleChange} />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Subject</Label>
+          <Control>
+            <Input name="subject" type="text" placeholder="What is the subject?" value={subject}
+              onChange={this.handleChange} />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Message</Label>
+          <Control>
+            <Textarea name="message" placeholder="Tell me more about..." value={message} cols='50' rows='5'
+              onChange={this.handleChange} />
+          </Control>
+        </Field>
+
+        <Field kind="group">
+          <Control> <Button color="dark">Send</Button> </Control>
+          <Control> <Button text>Cancel</Button> </Control>
+        </Field>
+      </form>
+    );
+  }
+}
+export default ContactForm;
--- src/index.js
+++ src/index.js
@@ -1,65 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import $ from 'jquery';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactForm from './contact';
 
-// window.addEventListener('load', () => {
-//   const preload = document.querySelector('.preload')
-//   preload.classList.add('preload-finish')
-// })
+const social = (
+  <div>
+  <nav>
+    <ul>
+      <li><a href="http://chengpham.com"> <i className="fas fa-globe-americas" /></a> </li>
+      <li><a href="http://github.com/chengpham"> <i className="fab fa-github" /></a> </li>
+      <li><a href="http://linkedin.com/in/chengpham"> <i className="fab fa-linkedin-in" /></a> </li>
+      <li><a href="mailto:chengpham@gmail.com"> <i className="fas fa-at" /></a> </li>
+    </ul>
+  </nav>
+  </div>
+)
+const preloader = (
+  <div id="preload">
+    <div id="loader"><img src="avatar_head.png" alt='avatar'/></div>
+  </div>
+)
 
 const slider = (
   <AwesomeSlider>
-    <div class="container-fluid">
-      <div class='app'>
-        <img id="avatar" src="./avatar.png"/>
+      <div className='app'>
         <h1>Hi, my name is Cheng Pham.</h1>
-        <h1>I build things for the web.</h1>
         <p>
+          I build things for the web.
           I am a full-stack web developer and UI/UX javascript specialist
           based in Vancouver, BC. I specialize in designing websites and application.
-          Check out projects on here to see my latest projects and live demos of the work I've done.
+          Come check out my latest projects and live demos of the work I've done.
         </p>
       </div>
-      
-    </div>
 
-    <div class="container-fluid">
-      <div class='app'>
-          <h1>My Portfolio</h1>
-      </div>
-    </div>
+      <div className='app'>
+          <h1>About Me</h1>
+          <div>
+          <p>
+          Hi, my name is Cheng and I am a software developer who recently graduated from CodeCore. I have always enjoyed building things, whether in the digital or physical realm. 
+          </p>
+          
+          <p>Skills</p>
+          <p>Programming Languages</p>
+          <p>TypeScript , JavaScript ES6 , HTML, (S)CSS , Ruby, SQL, Matlab</p>
 
-    <div class="container-fluid">
-      <div class='app'>
-          <h1>Contact Me</h1>
+          <p>Frameworks & Libraries
+          React , Redux, Next.js , Jest, Node.js, Express, Rails, JQuery</p>
+
+          <p>Tools & Platforms
+          Github, Firebase , Vercel, DatoCMS, Wordpress, Asana, Jira</p>
+
+          <p>Graphics & Design (Mechanical + Web)
+          Catia V5 , After Effects, Premiere, Photoshop, Figma</p>
+          </div>
       </div>
-    </div>
+
+
+      <div className='app'>
+          <h1>Portfolio</h1><br/> <h4>Wheel Spinner</h4>
+          <p><a href="https://chengs-wheel-spinner.herokuapp.com"><img src="wheel.png" width='400px' alt='wheel spinner'/></a></p>
+      </div>
+
+      <div className='app'>
+        <h1>Contact Me</h1>
+        <ContactForm/>
+      </div>
   </AwesomeSlider>
 );
+
 class App extends React.Component {
-  constructor(props){
-      super(props)
-  }
   render(){
       return (
-          <div id='main'>
+          <div>
+            {social}
             {slider}
-            <div id='nav'>
-              <nav>
-                <ul>
-                  <li><a href="http://chengpham.com"> <i class="fas fa-globe-americas"></i></a> </li>
-                  <li><a href="http://github.com/chengpham"> <i class="fab fa-github"></i></a> </li>
-                  <li><a href="http://linkedin.com/in/chengpham"> <i class="fab fa-linkedin-in"></i></a> </li>
-                  <li><a href="#"> <i class="fas fa-at"></i></a> </li>
-                </ul>
-              </nav>
-            </div>
-            <div class="preload">
-              <div class="loader"><img src="./avatar_head.png" /></div>
-            </div>
+           
           </div>
       )
   }
